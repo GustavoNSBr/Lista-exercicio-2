df50164c62973c4d83688224488794d3e81c8fee
GustavoNSBr <gustavonunessilvabr@gmail.com>
2023-09-15 20:30:45 -0300

    Lista-exercicios2

--- numerosPares.c
+++ numerosPares.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
  
-int main() {
-    int i;
+int main(){
 
-    for(i = 1, i <= 100,i++){
-        if(i%2 == 0){
-            printf("%d\n", i);
-        }
+    int i;
+    // usando For
+
+    // for(i = 1; i <= 100; i++){
+    //     if(i%2 == 0){
+    //         printf("%d\n", i);
+    //     }
+    // }
+
+    // SOLUCAO CHATGPT
+    for (i = 2; i <= 100; i += 2){
+        printf("%d\n", i);
     }
 
     return 0;
